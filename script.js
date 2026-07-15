11998a34759e23bb4e6f81fce852e2fb07c58039
rowan <angel.imddx@gmail.com>
2026-07-15 03:29:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Dashboard iniciado");
+
+document.getElementById("rules").addEventListener("click", function () {
+    alert("Work in progress.");
+});
+
+document.getElementById("lovelist").addEventListener("click", function () {
+    alert("Work in progress.");
+});
